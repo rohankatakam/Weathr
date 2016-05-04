25cfde4df44ff26522630ab35d954d07ea6aaf9f
Matthew Wen <mattwen2018@gmail.com>
2016-05-04 08:58:44 -0500

    fixed the subtitle

--- Weathr/UserOptions.swift
+++ Weathr/UserOptions.swift
@@ -12,6 +12,8 @@ class UserOptions: NSObject {
     
     var dailyNotification = "6:00am"
     var frequency = "1 hour"
+    var dailyNotificationSubTitle = "Set a time to be notified daily"
+    var freqencySubtitle = "Set how freqent you want to be notified"
     
     var switchState : Bool?
     

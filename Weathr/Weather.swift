88de2dd72bf70ee3fd5e001118fb29b0d6e30905
Rohan K <rohankatakam@yahoo.com>
2016-05-09 12:26:39 -0500

    Added Another Function to the Weather class

--- Weathr/Weather.swift
+++ Weathr/Weather.swift
@@ -10,21 +10,48 @@ import UIKit
 
 class Weather: NSObject {
     func temperature(latitude: Double, longitude: Double) -> Int{
+        
         let url = NSURL(string: "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=e4ca3ac79d18533cfdc17db5cced6263")
         let data = NSData(contentsOfURL: url!)
         
         do {
             var json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
-            
+
             if let weather = json["main"] as? Dictionary<String, Double> {
                 let temp = weather["temp"]
                 return Int(1.8 * (Double(temp!) - 273) + 32)
             }
+            
         } catch {
             print("error serializing JSON: \(error)")
         }
         return 0
     }
+    
+    
+    func description(latitude: Double, longitude: Double) -> String {
+        let url = NSURL(string: "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=e4ca3ac79d18533cfdc17db5cced6263")
+        
+        let data = NSData(contentsOfURL: url!)
+
+        do {
+            var json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+            
+            if let weather = json["weather"] as? [[String: AnyObject]] {
+                for state in weather {
+                    if let name = state["description"] as? String {
+                        return name
+                    }
+                }
+            }
+            
+        } catch {
+            print("error serializing JSON: \(error)")
+        }
+        return ""
+    }
+    
+    
     
     func city(latitude: Double, longitude: Double) -> String{
         var url = NSURL(string: "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=e4ca3ac79d18533cfdc17db5cced6263")
@@ -32,7 +59,6 @@ class Weather: NSObject {
         
         do {
             let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
-            
             if let weather = json["name"] as? String {
                 return String(weather)
             }
@@ -41,6 +67,4 @@ class Weather: NSObject {
         }
         return ""
     }
-    
-    
 }
